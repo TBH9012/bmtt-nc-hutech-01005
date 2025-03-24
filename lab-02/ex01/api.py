c11ad0468b48801b5699c3714a15612d8b5b1c87
TBH9012 <tranbinhhoa999@gmail.com>
2025-03-24 10:01:58 +0700

    [add] lab-03 ui caesar

--- lab-02/ex01/api.py
+++ lab-02/ex01/api.py
@@ -1,10 +1,31 @@
 from flask import Flask, request, jsonify
+from cipher.caesar import CaesarCipher
 from cipher.transposition import TranspositionCipher
 
 app = Flask(__name__)
 
 transposition_cipher = TranspositionCipher()
 
+# CAESAR CIPHER ALGORITHM
+caesar_cipher = CaesarCipher()
+
+@app.route("/api/caesar/encrypt", methods=["POST"])
+def caesar_encrypt():
+    data = request.json
+    plain_text = data['plain_text']
+    key = int(data['key'])
+    encrypted_text = caesar_cipher.encrypt_text(plain_text, key)
+    return jsonify({'encrypted_message': encrypted_text})
+
+@app.route("/api/caesar/decrypt", methods=["POST"])
+def caesar_decrypt():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = int(data['key'])
+    decrypted_text = caesar_cipher.decrypt_text(cipher_text, key)
+    return jsonify({'decrypted_message': decrypted_text})
+
+# TRANSPOSITION
 @app.route('/api/transposition/encrypt', methods=['POST'])
 def transposition_encrypt():
     data = request.get_json()
